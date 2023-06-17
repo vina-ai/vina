88bb6f0efb16857077485701e8b26d4511770e2f
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-06-17 14:00:29 +0900

    (master) parsing api response

--- crates/vina_story/src/api.rs
+++ crates/vina_story/src/api.rs
@@ -18,7 +18,7 @@ impl ApiClient {
     pub fn new(api_key: String) -> Self {
         Self {
             api_key,
-            model: String::from("gpt-3"),
+            model: String::from("gpt-3.5-turbo-0613"),
         }
     }
 
@@ -26,9 +26,27 @@ impl ApiClient {
         let client = reqwest::blocking::Client::new();
 
         let body: serde_json::Value = json!({
-         "model": "gpt-3.5-turbo",
-         "messages": [{"role": "user", "content": "Say this is a test!"}],
-         "temperature": 0.7
+            "model": self.model,
+            "messages": [
+                {"role": "user", "content": "What's the weather in Kitchener Waterloo Region Canada?"}
+            ],
+            "functions": [
+                {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather in a given location",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                        },
+                        "required": ["location"],
+                    }
+                }
+            ],
         });
 
         let res = client
@@ -38,7 +56,25 @@ impl ApiClient {
             .send()?
             .text()?;
 
-        println!("{:?}", res);
+        let res_val: Value = serde_json::from_str(&res)?;
+        println!("{}", serde_json::to_string_pretty(&res_val)?);
+
+        let choices = &res_val["choices"].as_array().unwrap();
+        for choice in choices.iter() {
+            let finish_reason = choice["finish_reason"].as_str().unwrap();
+            if finish_reason == "function_call" {
+                let fn_call = &choice["message"]["function_call"];
+                let fn_name = fn_call["name"].as_str().unwrap();
+                let fn_args = &fn_call["arguments"];
+                if fn_name == "get_current_weather" {
+                    println!("Calling {} with {:?}", fn_name, fn_args);
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn parse_response(&self, res: &Value) -> anyhow::Result<()> {
         Ok(())
     }
 }
